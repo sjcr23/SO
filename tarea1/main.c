419b7b0279b900193fbbddebe1fa0deca68a89bc
Josué Castro Ramírez <61713278+sjcr23@users.noreply.github.com>
2024-02-29 21:07:42 -0600

    Update main.c Se implementa el mutex al algoritmo paralelo

--- tarea1/main.c
+++ tarea1/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <pthread.h>
 #include "mergeSort.c"
-#include "parallel_mergeSort.c"
+
 
 #define MIN_NUM -1000
 #define MAX_NUM 1000
@@ -21,6 +21,14 @@ typedef struct {
     long *arr;
     int id;
 } CalcularModaArgs;
+
+// Structure for passing arguments to the sorting function
+typedef struct {
+    long *arr;
+    long left;
+    long right;
+} MergeSortArgs;
+
 
 void *calcularModa(void *args) {
     CalcularModaArgs* myArgs = (CalcularModaArgs*) args;
@@ -66,6 +74,90 @@ void *calcularModa(void *args) {
     pthread_exit(NULL);
 }
 
+
+// Function to merge two subarrays of arr[]
+void parallel_merge(long arr[], long l, long m, long r) {
+    long i, j, k;
+    long n1 = m - l + 1;
+    long n2 = r - m;
+
+    // Crear arreglos temporales
+    long L[n1], R[n2];
+
+    // Copiar datos a los arreglos temporales L[] y R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temporary arrays back into arr[l..r]
+    i = 0;
+    j = 0;
+    k = l;
+
+    
+    while (i < n1 && j < n2) {
+        pthread_mutex_unlock(&mutex);
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+        pthread_mutex_lock(&mutex);
+    }
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        pthread_mutex_unlock(&mutex);
+        arr[k] = L[i];
+        i++;
+        k++;
+        pthread_mutex_lock(&mutex);
+    }
+    
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        pthread_mutex_lock(&mutex);
+        arr[k] = R[j];
+        j++;
+        k++;
+        pthread_mutex_lock(&mutex);
+    }
+}
+
+// Parallel merge sort function
+void* parallel_mergeSort(void* args) {
+    MergeSortArgs* myArgs = (MergeSortArgs*) args;
+    long* arr = myArgs->arr;
+    long left = myArgs->left;
+    long right = myArgs->right;
+
+    if (left < right) {
+        // Encuentra el punto medio del arreglo
+        long mid = left + (right - left) / 2;
+
+        // Crea los hilos que para ordenar la izquierda y la derecha
+        pthread_t tid1, tid2;
+        MergeSortArgs args1 = {arr, left, mid};
+        MergeSortArgs args2 = {arr, mid + 1, right};
+                
+        pthread_create(&tid1, NULL, parallel_mergeSort, &args1);
+        pthread_create(&tid2, NULL, parallel_mergeSort, &args2);
+
+        // Wait for the threads to finish
+        pthread_join(tid1, NULL);
+        pthread_join(tid2, NULL);
+
+        // Merge the sorted subarrays
+        parallel_merge(arr, left, mid, right);
+    }
+    pthread_mutex_destroy(&mutex);
+    pthread_exit(NULL);
+}
 
 int main() {
     long N;
