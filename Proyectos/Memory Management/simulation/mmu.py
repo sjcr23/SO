47f0ce019bd52d9cc0c087b31356ad51f45b14f7
adam3497 <amador19972012@gmail.com>
2024-05-05 12:25:22 -0600

    Small fix so the pointer and PID can be mapped as PID -> ptr -> Pages

--- Proyectos/Memory Management/simulation/mmu.py
+++ Proyectos/Memory Management/simulation/mmu.py
@@ -12,8 +12,8 @@ class MemoryManagementUnit:
         self.page_size = page_size
         self.real_memory = [None] * (ram_size // page_size)
         self.virtual_memory = []
-        self.memory_map = {} # Memory map: pid -> pages
-        self.pointer_map = {} # Ptr -> pid
+        self.memory_map = {} # Memory map: ptr -> pages
+        self.pointer_map = {} # PID -> ptr
         self.pointer_counter = 1
 
     def execute_instruction(self, instruction):
@@ -30,6 +30,11 @@ class MemoryManagementUnit:
             print(f"Memory allocated for PID[{pid}] -> ptr:{ptr}")
         
     def allocate_memory(self, pid, size):
+        # Generate and assign a pointer to the process
+        ptr = self.generate_pointer()
+        self.pointer_map[ptr] = pid
+
+        # Calculate the number of pages to assign
         num_pages = size // self.page_size
         if size % self.page_size != 0:
             num_pages += 1
@@ -40,12 +45,7 @@ class MemoryManagementUnit:
             print("Page allocated: ", page.page_id, f" in real memory: {page.in_real_memory}")
             pages.append(page)
 
-        self.memory_map[pid] = pages
-
-        # Generate and assign a pointer to the process
-        ptr = self.generate_pointer()
-        self.pointer_map[ptr] = pid
-
+        self.memory_map[ptr] = pages
         return ptr
     
     def generate_pointer(self):
@@ -78,16 +78,14 @@ class MemoryManagementUnit:
 mmu = MemoryManagementUnit(400*1024, 4096)
 print("real memory ", len(mmu.real_memory))
 mmu.execute_instruction('new(1, 24576)')
-print("Memory map for pid 1: ", mmu.memory_map[1])
+print("Memory map for pid 1: ", mmu.memory_map[mmu.pointer_map[1]])
 mmu.delete_memory(1)
 if 1 in mmu.memory_map:
-    print("Memory map for pid 1: ", mmu.memory_map[1])
+    print("Memory map for pid 1: ", mmu.memory_map[mmu.pointer_map[1]])
 else:
     print("There is not memory associated to the PID 1")
 
 mmu.execute_instruction('new(2, 4096)')
 mmu.execute_instruction('new(3, 4096)')
 mmu.execute_instruction('new(4, 4096)')
-print("Memory map for pid 4: ", mmu.memory_map[4])
 mmu.execute_instruction('new(1, 24576)')
-print("Memory map for pid 1: ", mmu.memory_map[1])
